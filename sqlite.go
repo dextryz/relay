2b8ae9d2b659389c87d5af58bf8b56e92e3b92a2
Dexter May Cry <rvs.hello@pm.me>
2023-08-14 15:34:34 -0400

    merge filter author and ids into a single SQL query

--- sqlite.go
+++ sqlite.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 
@@ -55,86 +57,67 @@ func (s *relay) store(e nostr.Event) error {
 // Query database with a filter for a set of events, then push (SubId, Event) on the stream.
 func (s *relay) query(subId string, filter nostr.Filter, stream chan<- nostr.MessageEvent) error {
 
-	if len(filter.Authors) > s.limits.max_limit {
-		return ErrMaxLimit
+    var conditions []string
+	var args []interface{}
+
+	if len(filter.Ids) > 0 {
+
+        if len(filter.Ids) > s.limits.max_limit {
+            return ErrMaxLimit
+        }
+
+		placeholdersForIds := make([]string, len(filter.Ids))
+		for i, id := range filter.Ids {
+			placeholdersForIds[i] = "?"
+			args = append(args, id)
+		}
+		conditions = append(conditions, fmt.Sprintf("id IN (%s)", strings.Join(placeholdersForIds, ",")))
 	}
 
-	if len(filter.Ids) > s.limits.max_limit {
-		return ErrMaxLimit
+	if len(filter.Authors) > 0 {
+
+        if len(filter.Authors) > s.limits.max_limit {
+            return ErrMaxLimit
+        }
+
+		placeholdersForPubkeys := make([]string, len(filter.Authors))
+		for i, pubkey := range filter.Authors {
+			placeholdersForPubkeys[i] = "?"
+			args = append(args, pubkey)
+		}
+		conditions = append(conditions, fmt.Sprintf("pubkey IN (%s)", strings.Join(placeholdersForPubkeys, ",")))
 	}
 
-	for _, pub := range filter.Authors {
-
-		events, err := eventsByPubkey(s.db, pub)
-		if err != nil {
-			return err
-		}
-
-		for _, event := range events {
-			stream <- nostr.MessageEvent{
-				SubscriptionId: subId,
-				Event:          *event,
-			}
-		}
-
-		log.Printf("Pulled events with public key: [%s..%s]", pub[:5], pub[len(pub)-5:])
+	// If both lists are empty, this is an error or not meaningful.
+	if len(conditions) == 0 {
+		return fmt.Errorf("both ids and pubkeys are empty")
 	}
 
-	for _, id := range filter.Ids {
+    query := fmt.Sprintf("SELECT * FROM events WHERE %s", strings.Join(conditions, " AND "))
 
-		event, err := eventById(s.db, id)
-		if err != nil {
-			return err
-		}
-
-		stream <- nostr.MessageEvent{
-			SubscriptionId: subId,
-			Event:          *event,
-		}
-
-		log.Printf("Pulled event with ID: [%s..%s]", id[:5], id[len(id)-5:])
-	}
-
-	return nil
-}
-
-func eventById(db *sql.DB, id string) (*nostr.Event, error) {
-
-	event := &nostr.Event{}
-
-	row := db.QueryRow("SELECT id, pubkey, created_at, kind, content, sig FROM events WHERE id = ?", id)
-
-	err := row.Scan(&event.Id, &event.PubKey, &event.CreatedAt, &event.Kind, &event.Content, &event.Sig)
+    rows, err := s.db.Query(query, args...)
 	if err != nil {
-		return nil, err
+		return err
 	}
-
-	return event, nil
-}
-
-func eventsByPubkey(db *sql.DB, pubkey string) ([]*nostr.Event, error) {
+    defer rows.Close()
 
 	var events []*nostr.Event
-
-	rows, err := db.Query("SELECT id, pubkey, created_at, kind, content, sig FROM events WHERE pubkey = ?", pubkey)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
 
 	for rows.Next() {
 		var event nostr.Event
 		err := rows.Scan(&event.Id, &event.PubKey, &event.CreatedAt, &event.Kind, &event.Content, &event.Sig)
 		if err != nil {
-			return nil, err
+			return err
 		}
 		events = append(events, &event)
 	}
 
-	err = rows.Err()
-	if err != nil {
-		return nil, err
-	}
+    for _, event := range events {
+        stream <- nostr.MessageEvent{
+            SubscriptionId: subId,
+            Event:          *event,
+        }
+    }
 
-	return events, nil
+	return nil
 }
